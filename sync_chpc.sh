daa24358809b3f9ea041e7c8124b8c069dc21df9
Tom <earnestt1234@gmail.com>
2023-08-18 12:14:31 -0500

    update synced files

--- sync_chpc.sh
+++ sync_chpc.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 
+# $CHPC is an enivornment variable with login address
+LOGIN=$CHPC
 CHPC_PATH='/mnt/beegfs/scratch/tom.earnest/atn_cognition'
 
-rsync -av 'data' $CHPC:${CHPC_PATH}
-rsync -av --exclude="*.py" --exclude='*.R' --exclude='*.sh' $CHPC:${CHPC_PATH}/'main' '.'
+UPLOADS='uploads.txt'
+DOWNLOADS='downloads.txt'
+
+# upload
+rsync -avRr --files-from="${UPLOADS}" . $LOGIN:${CHPC_PATH}/.
+
+# download
+rsync -avRr --files-from="${DOWNLOADS}" "$LOGIN:${CHPC_PATH}/." '.'

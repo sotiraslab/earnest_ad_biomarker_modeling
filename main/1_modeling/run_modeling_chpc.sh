0168a0e2a0e0f010b4b67621d7c7ae9f9ba3ce41
Tom <earnestt1234@gmail.com>
2024-01-15 11:55:42 -0600

    fix bad bash argument

--- main/1_modeling/run_modeling_chpc.sh
+++ main/1_modeling/run_modeling_chpc.sh
@@ -20,7 +20,7 @@ do
     name=$(basename $script .py)
     log=${name}.slurmlog
 
-    COMMAND=(sbatch -J $name -o $log -t '72:00:00' -N 1 -n 1 --mem=128GB --account='aristeidis_sotiras' --partition=='tier2_cpu' call_python.sh $script)
+    COMMAND=(sbatch -J $name -o $log -t '72:00:00' -N 1 -n 1 --mem=128GB --account='aristeidis_sotiras' --partition='tier2_cpu' call_python.sh $script)
     echo "${COMMAND[@]}"
     "${COMMAND[@]}"
 

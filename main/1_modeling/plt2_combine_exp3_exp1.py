f5720792fdc33e8c5f049f8027f936ca46f1efcb
Tom <earnestt1234@gmail.com>
2024-03-13 15:13:52 -0500

    rename fig scripts to plt

---
+++ main/1_modeling/plt2_combine_exp3_exp1.py
@@ -0,0 +1,73 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Mar 13 09:54:56 2024
+
+@author: tom.earnest
+"""
+
+import os
+import warnings
+
+import matplotlib.pyplot as plt
+import pandas as pd
+
+from helpers import results_boxplot
+
+def main():
+    
+    this_dir = os.path.dirname(os.path.abspath(__file__))
+    exp3_folder = os.path.abspath(os.path.join(this_dir, '..', '..', 'outputs', 'exp3_combo_atn_models_global_cognition_vs_binary'))
+    exp1_folder = os.path.abspath(os.path.join(this_dir, '..', '..', 'outputs', 'exp1_svms_global_cognition'))
+    
+    exp3_results = os.path.join(exp3_folder, 'results.csv')
+    exp1_results = os.path.join(exp1_folder, 'results.csv')
+    
+    if not os.path.exists(exp3_results):
+        raise RuntimeError('Results for exp3 are missing.')
+        
+    if not os.path.exists(exp1_results):
+        short_folder = os.path.abspath(os.path.join(this_dir, '..', '..', 'outputs', 'exp1_svms_global_cognition_short'))
+        short_results = exp1_results = os.path.join(short_folder, 'results.csv')
+        
+        if os.path.exists(short_results):
+            warnings.warn('The full results are missing for exp1 (SVMs), but the short results are present.  Using the latter!',
+                          RuntimeWarning)
+            exp1_results = short_results
+        else:
+            raise RuntimeError('Results for exp1 are missing.')
+        
+    # concatenate data
+    exp3 = pd.read_csv(exp3_results)
+    exp1 = pd.read_csv(exp1_results)
+    concat = pd.concat([exp3, exp1])
+    concat = concat.loc[~concat['model'].str.contains('PVC')]
+        
+    # output
+    experiment_name = os.path.splitext(os.path.basename(__file__))[0]
+    output_folder = os.path.abspath(os.path.join(this_dir, '..', '..', 'outputs', experiment_name))
+    if not os.path.isdir(output_folder):
+        os.mkdir(output_folder)
+    
+    # general resources
+    plot_path = os.path.join(output_folder, 'boxplot.svg')
+    palette = (['gray'] +
+        ['#E59C9C'] * 3 +
+        ['#ba635d'] +
+        ['#AAC3E9'] * 3 +
+        ['#7DA1D8'] +
+        ['#B3E2AD'] * 3 +
+        ['#99C494'])
+    
+    # plot
+    n_train = concat['ntrain'].values[0]
+    n_test = concat['ntest'].values[0]
+    fig, _ = results_boxplot(concat, groupby='model', baseline='All binary',
+                             stats=True, palette=palette,
+                             n_train=n_train, n_test=n_test)
+    
+    plt.tight_layout()
+    fig.savefig(plot_path, dpi=300)
+    
+if __name__ == '__main__':
+    main()

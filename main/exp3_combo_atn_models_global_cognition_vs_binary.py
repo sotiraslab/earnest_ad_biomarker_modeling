81478940c6a81fe0f88885fc5cc67f7269a0f074
Tom <earnestt1234@gmail.com>
2024-04-22 15:42:47 -0500

    readd exp3, plt1, plt2

---
+++ main/exp3_combo_atn_models_global_cognition_vs_binary.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Mar  6 09:12:35 2024
+
+@author: tom.earnest
+"""
+
+import os
+
+import pandas as pd
+
+from atn_modeling.experiments import experiment_combo_atn_vs_binary
+from common import parse, setup_output
+
+def main(short=False):
+    
+    # NOTE: Short has no effect
+    # only included for consistency with other experiment scripts
+    
+    output_folder = setup_output(__file__)
+        
+    # EXPERIMENT PARAMETERS
+    parameters = dict(
+        dataset = pd.read_csv('datasets/maindata.csv'),
+        target = 'PHC_GLOBAL',
+        covariates=['Age', 'SexBinary', 'HasE4Binary'],
+        stratify='CDRBinned',
+        repeats=10,
+        outer_splits=10,
+        inner_splits=5,
+        outer_seed=0,
+        inner_seed=100,
+        savepath=os.path.join(output_folder, 'results.csv'),
+        savemodels=os.path.join(output_folder, 'models.pickle'),
+        savelms=os.path.join(output_folder, 'lm.pickle')
+        )
+     
+    _ = experiment_combo_atn_vs_binary(**parameters)
+    
+if __name__ == '__main__':
+    args = parse()
+    kwargs = vars(args)
+    main(**kwargs)
---
+++ main/plt2_boxplot_combo_models_vs_binary.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Mar 13 09:54:56 2024
+
+@author: tom.earnest
+"""
+
+import os
+import matplotlib.pyplot as plt
+import pandas as pd
+
+from atn_modeling.helpers import results_boxplot
+from common import load_results
+    
+# concatenate data
+exp3 = load_results('exp3_combo_atn_models_global_cognition_vs_binary', 'results.csv')
+exp1 = load_results('exp1_svms_global_cognition', 'results.csv')
+concat = pd.concat([exp3, exp1])
+concat = concat.loc[~concat['model'].str.contains('PVC')]
+    
+# output
+plot_path = os.path.join('figures', os.path.splitext(os.path.basename(__file__))[0] + '.svg')
+
+# general resources
+palette = (['gray'] +
+    ['#E59C9C'] * 3 +
+    ['#ba635d'] +
+    ['#AAC3E9'] * 3 +
+    ['#7DA1D8'] +
+    ['#B3E2AD'] * 3 +
+    ['#99C494'])
+
+# plot
+n_train = concat['ntrain'].values[0]
+n_test = concat['ntest'].values[0]
+fig, _ = results_boxplot(concat, groupby='model', baseline='All binary',
+                         stats_vs_baseline=True, palette=palette,
+                         n_train=n_train, n_test=n_test, font_file='arial.ttf')
+
+plt.tight_layout()
+fig.savefig(plot_path, dpi=300)

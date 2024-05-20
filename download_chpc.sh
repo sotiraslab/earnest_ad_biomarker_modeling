58a50308dbd6a87b2a93c50bd158ca9397ddfc92
Tom <earnestt1234@gmail.com>
2024-05-20 08:50:47 -0500

    split chpc syncing into upload/download

--- download_chpc.sh
+++ download_chpc.sh
@@ -9,12 +9,6 @@ echo "!! REMINDER !!"
 echo "    Make sure the CHPC repo is up to date (git pull)!"
 echo ""
 
-# upload
-# only the input data and processed tables are synced
-# rsync -av inputs $LOGIN:${CHPC_PATH}
-rsync -av "main/inputs/" "$LOGIN:${CHPC_PATH}/main/inputs"
-rsync -av "main/datasets/" "$LOGIN:${CHPC_PATH}/main/datasets"
-
 # download
 # outputs are downloaded
 rsync -av "$LOGIN:${CHPC_PATH}/main/outputs" 'main'
---
+++ upload_chpc.sh
@@ -0,0 +1,16 @@
+
+# $CHPC is an enivornment variable with login address
+LOGIN=$CHPC2
+CHPC_PATH='/mnt/beegfs/scratch/tom.earnest/atn_cognition'
+
+# reminder
+echo ""
+echo "!! REMINDER !!"
+echo "    Make sure the CHPC repo is up to date (git pull)!"
+echo ""
+
+# upload
+# only the input data and processed tables are synced
+# rsync -av inputs $LOGIN:${CHPC_PATH}
+rsync -av "main/inputs/" "$LOGIN:${CHPC_PATH}/main/inputs"
+rsync -av "main/datasets/" "$LOGIN:${CHPC_PATH}/main/datasets"

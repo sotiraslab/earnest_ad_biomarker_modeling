401373ea27a8a5ea378e132e5bab96be55565301
Tom Earnest <31780412+earnestt1234@users.noreply.github.com>
2026-07-10 11:21:19 -0500

    Add modeling of MMSE (2 year)

---
+++ main/pltR1_comboVSbinary_MMSE2year.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Mar 13 09:54:56 2024
+
+@author: tom.earnest
+"""
+
+import os
+import matplotlib.pyplot as plt
+import pandas as pd
+
+from atn_modeling.helpers import results_boxplot
+from common import load_results, set_labels_binary_exp
+
+# concatenate data
+biomarkers = load_results('expR1_comboVSbinary_MMSE2year', 'results.csv')
+svm = load_results('expR1_svm_MMSE2year', 'results.csv')
+concat = pd.concat([biomarkers, svm])
+concat = concat.loc[~concat['model'].str.contains('PVC')]
+
+# output
+plot_path = os.path.join('figures', os.path.splitext(os.path.basename(__file__))[0] + '.svg')
+
+# general resources
+palette = (['#F7A934'] +
+    ['#E899EE'] * 3 +
+    ['#B74CBF'] +
+    ['#F29D9D'] * 3 +
+    ['#FC4646'] +
+    ['#A5DBF2'] * 3 +
+    ['#08A3E5'])
+
+# plot
+# plt.rcParams['font.size'] = 10
+n_train = concat['ntrain'].values[0]
+n_test = concat['ntest'].values[0]
+fig, stats = results_boxplot(concat, groupby='model', baseline='All binary',
+                             stats_vs_baseline=True, palette=palette,
+                             n_train=n_train, n_test=n_test, font_file='arial.ttf',
+                             pivot_values='rmse')
+set_labels_binary_exp(fig)
+fig.gca().set_ylabel('Prediction Error for ΔMMSE (RMSE)')
+
+plt.tight_layout()
+fig.savefig(plot_path, dpi=300)
---
+++ main/pltR1_comboVSsolo_MMSE2year.py
@@ -0,0 +1,74 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Mar 13 09:54:56 2024
+
+@author: tom.earnest
+"""
+
+import os
+import matplotlib.pyplot as plt
+import pandas as pd
+
+from common import load_results, set_labels_baseline_exp
+from atn_modeling.helpers import results_boxplot
+
+# load data
+biomarkers = load_results('expR1_comboVSsolo_MMSE2year', 'results.csv')
+svm = load_results('expR1_svm_MMSE2year', 'results.csv')
+
+# concatenate
+concat = pd.concat([biomarkers, svm])
+concat = concat.loc[~concat['model'].str.contains('PVC')]
+
+# output
+plot_path = os.path.join('figures', os.path.splitext(os.path.basename(__file__))[0] + '.svg')
+
+# general resources
+palette = (['gray'] +
+    ['#FFDDAA'] * 3 +
+    ['#F7A934'] +
+    ['#E899EE'] * 3 +
+    ['#B74CBF'] +
+    ['#F29D9D'] * 3 +
+    ['#FC4646'] +
+    ['#A5DBF2'] * 3 +
+    ['#08A3E5'])
+
+# pairwise comparisons
+pairs = [
+    ('All binary', 'Binary A'),
+    ('All binary', 'Binary T'),
+    ('All binary', 'Binary N'),
+    ('All categorical', 'Categorical A'),
+    ('All categorical', 'Categorical T'),
+    ('All categorical', 'Categorical N'),
+    ('All continuous', 'Continuous A'),
+    ('All continuous', 'Continuous T'),
+    ('All continuous', 'Continuous N'),
+    ('ATN SVM', 'Amyloid SVM'),
+    ('ATN SVM', 'Tau SVM'),
+    ('ATN SVM', 'GM SVM'),
+    ('Amyloid SVM', 'Continuous A'),
+    ('Tau SVM', 'Continuous T'),
+    ('GM SVM', 'Continuous N'),
+    ('ATN SVM', 'All continuous'),
+    ]
+
+positions = [1.0, 1.05, 1.1] * 4 + [1.15, 1.2, 1.25]
+
+# plot
+# plt.rcParams['font.size'] = 10
+n_train = concat['ntrain'].values[0]
+n_test = concat['ntest'].values[0]
+fig, stats = results_boxplot(concat, groupby='model', baseline='Baseline',
+                         stats_vs_baseline=True, palette=palette,
+                         n_train=n_train, n_test=n_test, font_file='arial.ttf',
+                         stats_pairs=pairs, stats_pairs_positions=positions)
+set_labels_baseline_exp(fig)
+
+# update text
+fig.gca().set_ylabel('Prediction Error for ΔMMSE (RMSE)')
+
+plt.tight_layout()
+fig.savefig(plot_path, dpi=300)
